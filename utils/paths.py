9a892315c9db1ed84d49720156626422a3758448
Dmitrijs Voronovs <dmitrijs.voronovs@sapiens.com>
2024-07-17 13:06:44 +0200

    Update filename

--- utils/paths.py
+++ utils/paths.py
@@ -1,5 +1,5 @@
 class Paths:
-    GOLDEN_REPOS = 'cfg/golder-repositories.txt'
+    GOLDEN_REPOS = 'cfg/golden-repositories.txt'
     TAGS = 'cfg/tags.txt'
     TAGS_TO_ELIMINATE = 'cfg/tags-to-eliminate.txt'
     TAGS_EXTRACTED = 'cfg/tags-extracted.txt'
