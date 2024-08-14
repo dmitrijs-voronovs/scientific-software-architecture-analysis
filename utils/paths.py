36ce5bc17e01a261c2d047a5a4379fde5f2d859c
Dmitrijs <Dmitrijs.Voronovs@sapiens.com>
2024-08-14 13:32:51 +0200

    Update topics file name

--- utils/paths.py
+++ utils/paths.py
@@ -2,6 +2,6 @@ class Paths:
     GOLDEN_REPOS = 'cfg/golden-repositories.txt'
     TAGS = 'cfg/topics.txt'
     TAGS_TO_ELIMINATE = 'cfg/topics-to-exclude.txt'
-    TAGS_EXTRACTED = 'cfg/topics-extracted.txt'
+    TAGS_EXTRACTED = 'cfg/topics-raw.txt'
     CACHED_CATEGORY_CURSORS = '.cache/category_cursors.json'
     CACHED_CATEGORY_CURSORS_isOrganization = '.cache/category_cursors_addition.json'
