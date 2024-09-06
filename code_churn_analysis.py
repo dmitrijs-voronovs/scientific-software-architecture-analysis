367661a507881c6f44589306a4b3e7659e56ca66
Dmitrijs <Dmitrijs.Voronovs@sapiens.com>
2024-09-06 11:13:54 +0200

    Add churn graph (test)

--- code_churn_analysis.py
+++ code_churn_analysis.py
@@ -6,6 +6,7 @@ from collections import defaultdict
 import networkx as nx
 import matplotlib.pyplot as plt
 from datetime import datetime
+import plotly.graph_objects as go
 
 
 def run_git_command(repo_path, command):
@@ -63,6 +64,62 @@ def visualize_churn_graph(G):
     plt.savefig("churn_graph.png")
 
 
+def visualize_churn_graph_with_plotly(G):
+    edge_x = []
+    edge_y = []
+    for edge in G.edges():
+        x0, y0 = G.nodes[edge[0]]['churn'], G.nodes[edge[0]]['modified']
+        x1, y1 = G.nodes[edge[1]]['churn'], G.nodes[edge[1]]['modified']
+        edge_x.extend([x0, x1, None])
+        edge_y.extend([y0, y1, None])
+
+    node_x = []
+    node_y = []
+    node_text = []
+    for node in G.nodes():
+        x, y = G.nodes[node]['churn'], G.nodes[node]['modified']
+        node_x.append(x)
+        node_y.append(y)
+        node_text.append(node)
+
+    fig = go.Figure()
+
+    fig.add_trace(go.Scatter(
+        x=edge_x, y=edge_y,
+        line=dict(width=0.5, color='gray'),
+        hoverinfo='none',
+        mode='lines'))
+
+    fig.add_trace(go.Scatter(
+        x=node_x, y=node_y,
+        mode='markers',
+        hoverinfo='text',
+        marker=dict(
+            showscale=True,
+            colorscale='YlGnBu',
+            reversescale=True,
+            color=[],
+            size=10,
+            colorbar=dict(
+                thickness=15,
+                title='Churn',
+                xanchor='left',
+                titleside='right'
+            ),
+            line_width=2)))
+
+    fig.update_layout(
+        title='Code Churn Graph',
+        title_x=0.5,
+        showlegend=False,
+        hovermode='closest',
+        margin=dict(b=20, l=5, r=5, t=40),
+        xaxis=dict(showgrid=False, zeroline=False, showticklabels=False),
+        yaxis=dict(showgrid=False, zeroline=False, showticklabels=False))
+
+    fig.show()
+
+
 def interactive_churn_table(churn_data, times_modified):
     df = pd.DataFrame({'file': list(churn_data.keys()), 'churn': list(churn_data.values()),
                        'times_modified': list(times_modified.values()),
@@ -90,8 +147,9 @@ def store_churn_data(project_name, version, churn_data):
 def main():
     repo_path = ".tmp/scverse/scanpy/master"
     churn_data, times_modified = analyze_code_churn(repo_path)
-    # churn_graph = create_churn_graph(churn_data, times_modified)
+    churn_graph = create_churn_graph(churn_data, times_modified)
     # visualize_churn_graph(churn_graph)
+    # visualize_churn_graph_with_plotly(churn_graph)
     interactive_churn_table(churn_data, times_modified)
 
     # Store the churn data in MongoDB
