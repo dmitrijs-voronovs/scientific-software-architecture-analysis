849d3e19f1403a673764db2ff2744d80c66faf9d
Dmitrijs <Dmitrijs.Voronovs@sapiens.com>
2024-11-14 12:41:08 +0100

    Create a keyword validation script by querying local ollama model. Update prompt

--- split_csv.py
+++ split_csv.py
@@ -1,21 +1,23 @@
 import math
+from itertools import islice
 from pathlib import Path
 
 import pandas as pd
 from tqdm import tqdm
 
-MAX_FILE_SIZE_MB = 90 * 1000 * 1000
+MAX_FILE_SIZE_BYTES = 90 * 1000 * 1000
 
 
-def split_file(path: str | Path):
+def split_file(path: str | Path, size_bytes=MAX_FILE_SIZE_BYTES):
     file_path = Path(path)
     content = pd.read_csv(file_path)
     file_size = file_path.stat().st_size
     num_rows = content.shape[0]
-    num_chunks = math.ceil(file_size / MAX_FILE_SIZE_MB)
+    num_chunks = math.ceil(file_size / size_bytes)
     print(f"Splitting file {path} of size {file_size} into {num_chunks} chunks")
-    chunk_size = math.ceil(num_rows / num_chunks)
-    chunks = [content.iloc[i:i + chunk_size] for i in tqdm(range(0, num_rows, chunk_size), desc=f"Splitting chunks of {file_path}")]
+    chunk_size_rows = math.ceil(num_rows / num_chunks)
+    chunks = [content.iloc[ran] for ran in tqdm(grouper_ranges(num_rows, chunk_size_rows), desc=f"Splitting chunks of {file_path}")]
+    print(chunks)
     for i, chunk in enumerate(chunks):
         chunk.to_csv(file_path.with_stem(f"{file_path.stem}.{i}"), index=False)
 
@@ -23,10 +25,17 @@ def split_file(path: str | Path):
 def check_file_sizes():
     for file_path in Path("metadata/keywords/").glob("*.csv"):
         size = file_path.stat().st_size
-        if size > MAX_FILE_SIZE_MB:
+        if size > MAX_FILE_SIZE_BYTES:
             print(f"File size: {file_path} | {size}")
             split_file(file_path)
 
 
+def grouper_ranges(total_size, chunk_size):
+    return (range(i, min(i + chunk_size, total_size)) for i in range(0, total_size, chunk_size))
+
+
+
 if __name__ == "__main__":
-    check_file_sizes()
+    # check_file_sizes()
+    print(list(grouper_ranges(98, 10)))
+    split_file("metadata/keywords/broadinstitute.cromwell.87.ISSUE_COMMENT.csv", 500_000)
--- validate_keyword_selection.py
+++ validate_keyword_selection.py
@@ -1,13 +1,15 @@
 import json
 import os
 import re
+import shelve
+from datetime import datetime
 from pathlib import Path
 
 import dotenv
-import google.generativeai as genai
 import pandas as pd
 import requests
-from tenacity import retry, wait_exponential, stop_after_attempt, RetryError, wait_incrementing
+from loguru import logger
+from tenacity import retry, stop_after_attempt, RetryError, wait_incrementing, wait_fixed
 from tqdm import tqdm
 
 # Load environment variables from .env file
@@ -20,27 +22,15 @@ YOUR_APP_NAME = "Keyword Selection"
 
 
 def request_llm(prompt):
-    response = requests.post(
-        url="https://openrouter.ai/api/v1/chat/completions",
-        headers={
-            "Authorization": f"Bearer {OPENROUTER_API_KEY}",
-            "X-Title": f"{YOUR_APP_NAME}",  # Optional. Shows in rankings on openrouter.ai.
-        },
-        data=json.dumps({
-            "model": "google/gemma-2-9b-it:free",  # Optional
-            "messages": [
-                {
-                    "role": "system",
-                    "content": "You are a helpful assistant who provides thoughtful and concise responses and tightly follows user instroctuctions. You never output any extra information."
-                },
-                {
-                    "role": "user",
-                    "content": prompt
-                }
-            ]
+    response = requests.post(url="https://openrouter.ai/api/v1/chat/completions",
+        headers={"Authorization": f"Bearer {OPENROUTER_API_KEY}", "X-Title": f"{YOUR_APP_NAME}",
+            # Optional. Shows in rankings on openrouter.ai.
+        }, data=json.dumps({"model": "google/gemma-2-9b-it:free",  # Optional
+            "messages": [{"role": "system",
+                "content": "You are a helpful assistant who provides thoughtful and concise responses and tightly follows user instroctuctions. You never output any extra information."},
+                {"role": "user", "content": prompt}]
 
-        })
-    )
+        }))
     return response.json()
 
 
@@ -61,11 +51,8 @@ def get_resp(index, row):
     return r
 
 
-@retry(
-    stop=stop_after_attempt(8),
-    wait=wait_incrementing(10, 10),
-    before_sleep=lambda retry_state: print(retry_state),
-)
+@retry(stop=stop_after_attempt(8), wait=wait_incrementing(10, 10),
+    before_sleep=lambda retry_state: print(retry_state), )
 def request_google_ailab(model, prompt):
     response = model.generate_content(prompt)
     text_resp = re.sub(r'```json|```', "", response.text)
@@ -74,31 +61,128 @@ def request_google_ailab(model, prompt):
     return json_resp['false_positive'], json_resp['reasoning']
 
 
-def main():
-    file_path = Path("./metadata/keywords/verification/big_sample2.csv")
+LOCAL_LLM_HOST = "http://localhost:11434"
 
-    genai.configure(api_key=os.getenv("GOOGLE_AI_STUDIO_KEY"))
-    model = genai.GenerativeModel("gemini-1.5-flash")
 
-    # df = pd.read_csv(file_path).sample(10)
-    df = pd.read_csv(file_path)
-    res = []
-    res_filename = "big_sample2_with_responses"
-    for index, row in tqdm(df.iterrows(), total=df.shape[0]):
+@retry(stop=stop_after_attempt(6), wait=wait_fixed(3), after=lambda retry_state: logger.warning(retry_state),
+    reraise=True, )
+def request_gemma(prompt):
+    url = "%s/api/generate" % LOCAL_LLM_HOST
+
+    payload = json.dumps({"model": "gemma", "prompt": prompt, "stream": False})
+    response = requests.request("POST", url, headers={'Content-Type': 'application/json'}, data=payload).json()
+
+    try:
+        text_resp = re.sub(r'```json|```', "", response['response'])
+        json_resp = json.loads(text_resp)
+        return json_resp['false_positive'], json_resp['reasoning']
+    except Exception as e:
+        raise Exception(f"Error in response: {response['response']}", e)
+
+
+quality_attribs = {"Availability": {
+    "desc": "The system's readiness to perform its function when required, focusing on reliability and recovery. It involves fault masking or repair to prevent failures, ensuring minimal cumulative downtime."},
+    "Deployability": {
+        "desc": "The capability of software to be deployed into an operational environment with predictable time and effort, including options for rollback if needed. Key aspects include automation, deployment speed, and deployment granularity."},
+    "Energy Efficiency": {
+        "desc": "The system’s ability to optimize resource use and minimize energy consumption while achieving required performance. This involves monitoring, allocation, and adaptation of resources."},
+    "Integrability": {
+        "desc": "The ease of combining the system with other systems or components, measured by integration cost and technical risks. Integrability considers the complexity and compatibility of interfaces, including syntactic, semantic, behavioral, and temporal alignment."},
+    "Modifiability": {
+        "desc": "The ease with which the system can be adapted by adding, removing, or modifying features, or adjusting to new environments. This attribute involves assessing the time, cost, and impact of changes, considering factors like coupling, cohesion, and the scope of modifications."},
+    "Performance": {
+        "desc": "The system’s capacity to meet its timing requirements, managing event handling and response times effectively. Performance focuses on reducing blocked time from resource contention and optimizing resource utilization under varying load conditions."},
+    "Safety": {
+        "desc": "The system’s ability to avoid states that could lead to harm or damage. Safety encompasses detection and handling of errors (e.g., omissions, timing, incorrect values) to prevent hazardous outcomes or mitigate potential damage."},
+    "Security": {
+        "desc": "The system’s ability to safeguard information against unauthorized access, while permitting authorized access. Security emphasizes confidentiality, integrity, and availability, using tactics to detect, prevent, and respond to attacks."},
+    "Testability": {
+        "desc": "The ease of validating software functionality through testing, enabling fault detection. This includes controlling and observing the system’s state, reducing complexity, and facilitating the creation of test cases and oracles."},
+    "Usability": {
+        "desc": "The degree to which users can effectively and efficiently accomplish tasks, including support for error recovery and user satisfaction. Usability covers ease of learning, efficient usage, and adaptability to user needs."}}
+
+to_prompt = lambda x: f"""
+You are an expert in evaluating and categorizing quality attributes in software engineering. You possess the necessary skills to distinguish sentences that clearly relate to a given quality attribute from those that do not. 
+
+Evaluate whether the content accurately aligns with its associated quality attribute, given the context provided. Your goal is to determine if the content makes sense in relation to the quality attribute description or if it is a false positive.
+
+Data:
+
+Quality Attribute: {x['quality_attribute']}
+Attribute Description: {x['attribute_desc']}
+Content: {x['sentence']}
+
+Instructions: 
+1. Analyze the content and the attribute description.
+2. Determine if the content accurately reflects the intended quality attribute in this context.
+3. If it does, label it as an accurate match; if not, mark it as a false positive.
+4. Output only the JSON object in response, without any additional explanation.
+5. Ensure the JSON output is properly formatted. Escape any special characters or inner quotes in strings to ensure compatibility with JSON parsers. Within JSON strings use \\\" to escape double quotes.
+
+Output your response as a JSON object in the following format:
+{{
+  "false_positive": <boolean>,
+  "reasoning": "<str>"
+}}
+"""
+
+
+def verify_file(file_path, res_filepath, batch_size=10):
+    os.makedirs(".cache/verification", exist_ok=True)
+    with shelve.open(f".cache/verification/{file_path.stem}", writeback=True) as db:
+        if 'processed' not in db:
+            db["processed"] = set()
+
+        if file_path.stem in db["processed"]:
+            logger.info(f"File {file_path.stem} already processed")
+            return
+
+        # genai.configure(api_key=os.getenv("GOOGLE_AI_STUDIO_KEY"))
+        # model = genai.GenerativeModel("gemini-1.5-flash")
+
         try:
-            r = request_google_ailab(model, row["prompt"])
-            res.append(r)
+            df = pd.read_csv(file_path)
+        except Exception as e:
+            logger.info(e)
+            return
 
-            if (index + 1) % 2 == 0:
-                df_to_save = df.iloc[0:index + 1].copy()
-                df_to_save['false_positive'], df_to_save['reasoning'] = zip(*res)
-                df_to_save.to_csv(file_path.with_stem(res_filename), index=False)
-        except RetryError as e:
-            print(f"Error in row {index}, {e}")
-            exit(1)
+        df["attribute_desc"] = df["quality_attribute"].apply(lambda x: quality_attribs[x]["desc"])
+        df['prompt'] = df.apply(lambda x: to_prompt(x), axis=1)
+        res = []
+        for i, (_idx, row) in tqdm(enumerate(df.iterrows()), total=df.shape[0], desc=f"Verifying {file_path.stem}"):
+            try:
+                # r = request_google_ailab(model, row["prompt"])
+                try:
+                    r = request_gemma(row["prompt"])
+                    res.append(r)
+                except Exception as e:
+                    logger.error(e)
+                    res.append((None, str(e)))
 
-    df['false_positive'], df['reasoning'] = zip(*res)
-    df.to_csv(file_path.with_stem(res_filename), index=False)
+                if (i + 1) % batch_size == 0:
+                    df_to_save = df.iloc[0:i + 1].copy()
+                    df_to_save['false_positive'], df_to_save['reasoning'] = zip(*res)
+                    df_to_save.to_csv(res_filepath, index=False)
+            except RetryError as e:
+                print(f"Error in row {i}, {e}")
+                exit(1)
+        df['false_positive'], df['reasoning'] = zip(*res)
+        df.to_csv(res_filepath, index=False)
+
+        db['processed'].add(file_path.stem)
+
+
+def main():
+    # os.makedirs(".logs", exist_ok=True)
+    logger.add(f".logs/verification.{datetime.now().isoformat()}.log", mode="w")
+    # file_path = Path("./metadata/keywords/verification/big_sample2.csv")
+    try:
+        file_folder = Path("metadata/keywords/")
+        for file_path in file_folder.glob("*.csv"):
+            res_filepath = file_path.parent / f"verification/{file_path.stem}.verified.csv"
+            verify_file(file_path, res_filepath)  # res_filepath = file_path.with_stem("test123")
+    except Exception as e:
+        logger.error(e)
 
 
 if __name__ == "__main__":
