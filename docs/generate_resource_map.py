701d5705326750007019b50fb0dfda9c7dde9c3f
Dmitrijs <Dmitrijs.Voronovs@sapiens.com>
2024-11-15 12:44:28 +0100

    fixup! Add github actions for dynamically generating sources for github pages charts and csv's
    Add github actions for dynamically generating sources for github pages charts and csv's

---
+++ docs/generate_resource_map.py
@@ -0,0 +1,20 @@
+from pathlib import Path
+
+
+def generate_resource_map():
+    current_dir = Path(__file__).parent
+    csvFiles = list(x.name for x in (current_dir / "csv").glob("*.csv"))
+    charts = list(x.name for x in (current_dir / "keyword_analysis").glob("*.html"))
+    validationCharts = list(x.name for x in (current_dir / "keyword_analysis" / "validation").glob("*.html"))
+    map = f'''
+const csvFiles = {csvFiles}  
+const charts = {charts} 
+const validationCharts = {validationCharts} 
+    '''
+    with open(current_dir / "resource-paths.js", "w") as f:
+        f.write(map)
+
+
+if __name__ == "__main__":
+    generate_resource_map()
+
