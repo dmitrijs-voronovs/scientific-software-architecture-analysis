3d41397ac5897b6b496d0854139d84f9cf82e723
Dmitrijs <Dmitrijs.Voronovs@sapiens.com>
2024-11-26 21:58:13 +0100

    Update docs, remove deplot git workflow

--- docs/generate_resource_map.py
+++ docs/generate_resource_map.py
@@ -6,10 +6,12 @@ def generate_resource_map():
     csv_files = list(x.name for x in (current_dir / "csv").glob("*.csv"))
     charts = list(x.name for x in (current_dir / "keyword_analysis").glob("*.html"))
     verification_charts = list(x.name for x in (current_dir / "keyword_analysis" / "verification").glob("*.html"))
+    verification_it2_charts = list(x.name for x in (current_dir / "keyword_analysis" / "verification_it2").glob("*.html"))
     mapping = f'''
 const csv_files = {csv_files}  
 const charts = {charts} 
 const verification_charts = {verification_charts} 
+const verification_it2_charts = {verification_it2_charts}
     '''
     with open(current_dir / "resource-paths.js", "w") as f:
         f.write(mapping)
@@ -17,4 +19,3 @@ const verification_charts = {verification_charts}
 
 if __name__ == "__main__":
     generate_resource_map()
-
