05f2a89390416f85bf16f7eb4b57157afd48359c
Dmitrijs <Dmitrijs.Voronovs@sapiens.com>
2025-02-14 10:39:07 +0100

    Add tactic first pass

--- constants/foldernames.py
+++ constants/foldernames.py
@@ -2,3 +2,5 @@ class FolderNames():
     VERIFICATION_DIR = "verification_v3"
     OPTIMIZED_KEYWORD_DIR = "optimized_v2"
     ARCHITECTURE_VERIFICATION_DIR = "verification_v3_architecture"
+    ARCHITECTURE_RELATED_MERGED_DIR = "verification_v3_architecture_related_merged"
+    CATEGORIZATION_DIR = "verification_v3_categorization"
