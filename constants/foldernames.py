57d1bf22718469c7764ca1740338dfbd79d35bb1
Dmitrijs <Dmitrijs.Voronovs@sapiens.com>
2025-03-03 17:16:55 +0100

    Rename verified files folder to a shorter name

--- constants/foldernames.py
+++ constants/foldernames.py
@@ -1,7 +1,7 @@
 class FolderNames():
+    KEYWORDS_RAW = "original"
     VERIFICATION_DIR = "verification_v3"
     OPTIMIZED_KEYWORD_DIR = "optimized_v2"
-    # ARCHITECTURE_VERIFICATION_DIR = "verification_v3_architecture"
-    ARCHITECTURE_VERIFICATION_DIR = "verification_v3_architecture_deepseek"
-    ARCHITECTURE_RELATED_MERGED_DIR = "verification_v3_architecture_related_merged"
+    ARCHITECTURE_VERIFICATION_DIR = "verification_v3_architecture"
+    ARCHITECTURE_TACTICS = "architecture_tactics"
     CATEGORIZATION_DIR = "verification_v3_categorization"
