b79971a63fdfda35262fe88d4a3111cbbcb774c3
Dmitrijs <Dmitrijs.Voronovs@sapiens.com>
2025-04-08 14:18:53 +0200

    Update processing stage names

--- constants/foldernames.py
+++ constants/foldernames.py
@@ -1,8 +1,8 @@
 class FolderNames():
     KEYWORDS_RAW = "original"
-    VERIFICATION_DIR = "verification_v3"
+    VERIFICATION_DIR = "verification"
     OPTIMIZED_KEYWORD_DIR = "optimized"
-    ARCHITECTURE_VERIFICATION_DIR = "verification_v3_architecture"
+    ARCHITECTURE_VERIFICATION_DIR = "verification_architecture"
     FORMAT_VALIDATION_DIR = "validate_format"
     ARCHITECTURE_TACTICS = "architecture_tactics_v3"
     ARCHITECTURE_TACTICS_v2 = "architecture_tactics_v2"
