41a99e2fa36bfb528ee4aac974b4541832fdc36a
Dmitrijs <Dmitrijs.Voronovs@sapiens.com>
2025-04-12 02:39:26 +0200

    Add word count to glue script

--- glue.py
+++ glue.py
@@ -31,6 +31,11 @@ def save_data(df, target_filename: Path):
     os.makedirs(target_filename.parent, exist_ok=True)
     df.to_excel(target_filename, index=False)
 
+def add_word_count(df, field_name = "sentence"):
+    df["word_count"] = df[field_name].apply(lambda x: len(re.sub(r"[\W_]+", " ", x).strip().split()))
+    return df
+
+
 def main():
     keywords_dir = Path(f"metadata/keywords/{FolderNames.FORMAT_VALIDATION_DIR}")
     # target_filename = Path(f"metadata/results/sample_{FolderNames.FORMAT_VALIDATION_DIR}.csv")
@@ -38,6 +43,7 @@ def main():
 
     try:
         df = get_data(keywords_dir)
+        df = add_word_count(df)
         save_data(df, target_filename)
     except Exception as error:
         print(f"Error processing {error=}\n{traceback.format_exc()}")
