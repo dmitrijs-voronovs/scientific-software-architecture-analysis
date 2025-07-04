d4cb8fb1a3fe5eea4954c5b7af7da0f1add9b0a0
Dmitrijs <Dmitrijs.Voronovs@sapiens.com>
2025-07-04 03:55:30 +0200

    Add data analysis for keyword matching stage

--- constants/foldernames.py
+++ constants/foldernames.py
@@ -5,3 +5,9 @@ class FolderNames:
     QA_RELEVANCE_CHECK_DIR = "s1_qa_relevance_check"
     ARCH_RELEVANCE_CHECK_DIR = "s2_arch_relevance_check"
     TACTIC_EXTRACTION_DIR = "s3_tactic_extraction"
+
+    RES_KEYWORDS_MATCHING_DIR = "keyword_matching"
+    RES_NOISE_FILTERING_DIR = "s0_noise_filtering"
+    RES_QA_RELEVANCE_CHECK_DIR = "s1_qa_relevance_check"
+    RES_ARCH_RELEVANCE_CHECK_DIR = "s2_arch_relevance_check"
+    RES_TACTIC_EXTRACTION_DIR = "s3_tactic_extraction"
