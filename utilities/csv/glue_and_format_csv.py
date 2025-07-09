49eb80ef837f2273b981f47475906c9d6b4083d8
Dmitrijs <Dmitrijs.Voronovs@sapiens.com>
2025-07-09 23:15:48 +0200

    Rename utility

--- utilities/csv/glue_and_format_csv.py
+++ utilities/csv/glue_and_format_csv.py
@@ -1,15 +1,13 @@
 import os
 import traceback
-from pathlib import Path
 
 import pandas as pd
 from tqdm import tqdm
 
 from constants.abs_paths import AbsDirPath
-from constants.foldernames import FolderNames
 from processing_pipeline.keyword_matching.model.MatchSource import MatchSource
 from cfg.selected_repos import selected_repos
-from split_csv import split_files_exceeding_max_limit
+from utilities.split_parquet import split_files_exceeding_max_limit
 
 
 
