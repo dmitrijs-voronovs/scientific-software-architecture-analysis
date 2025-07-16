1dda08ce15bd7df794a38684e72e4c3d0bac94f2
dmitrijs-voronovs <vdmitryy@gmail.com>
2025-07-16 12:49:54 +0200

    update runner 2 configuration

--- processing_pipeline/s0_noise_filtering/runner_2.py
+++ processing_pipeline/s0_noise_filtering/runner_2.py
@@ -1,10 +1,9 @@
 from cfg.LLMHost import LLMHost
-from cfg.ModelName import ModelName
 from processing_pipeline.s0_noise_filtering.NoiseFiltering import NoiseFilteringStage
 
 
 def main():
-    NoiseFilteringStage(hostname=LLMHost.SERVER, model_name_override=ModelName.DEEPSEEK_1_5B).execute(["root-project"], reverse=True)
+    NoiseFilteringStage(hostname=LLMHost.SERVER).execute(["root-project"], reverse=True)
 
 
 if __name__ == "__main__":
