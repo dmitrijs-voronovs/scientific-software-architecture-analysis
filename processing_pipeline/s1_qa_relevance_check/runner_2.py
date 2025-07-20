359650838618d89a2d2383d68b4aec51b22fac0f
dmitrijs-voronovs <vdmitryy@gmail.com>
2025-07-20 21:48:06 +0200

    Update runner

--- processing_pipeline/s1_qa_relevance_check/runner_2.py
+++ processing_pipeline/s1_qa_relevance_check/runner_2.py
@@ -3,7 +3,7 @@ from processing_pipeline.s1_qa_relevance_check.QARelevanceCheck import QARelevan
 
 
 def main():
-    QARelevanceCheckStage(hostname=LLMHost.RADU_SERVER).execute(["root-project.root.v6-32-06.code_comment.","root-project.root.v6-32-06.docs.","root-project.root.v6-32-06.issue_comment."], reverse=True)
+    QARelevanceCheckStage(hostname=LLMHost.SERVER).execute(["root-project.root.v6-32-06.code_comment.","root-project.root.v6-32-06.docs.","root-project.root.v6-32-06.issue_comment."], reverse=True)
 
 
 if __name__ == "__main__":
