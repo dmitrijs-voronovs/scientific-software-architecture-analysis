da654fe1cda307bb705909b661cecd47bb6be40f
dmitrijs-voronovs <vdmitryy@gmail.com>
2025-07-25 11:10:20 +0200

    Finish stage processing

--- exec_stage.sh
+++ exec_stage.sh
@@ -7,8 +7,8 @@
 #nohup python3 -m processing_pipeline.s2_arch_relevance_check.ArchRelevanceCheck > data/processing_logs/s2_greenlab.txt 2>&1 &
 #nohup python3 -m processing_pipeline.s2_arch_relevance_check.runner_2 > data/processing_logs/s2_techlab.txt 2>&1 &
 
-nohup python3 -m processing_pipeline.s3_tactic_extraction.TacticExtraction > data/processing_logs/s3_greenlab.txt 2>&1 &
-nohup python3 -m processing_pipeline.s3_tactic_extraction.runner_2 > data/processing_logs/s3_techlab.txt 2>&1 &
+#nohup python3 -m processing_pipeline.s3_tactic_extraction.TacticExtraction > data/processing_logs/s3_greenlab.txt 2>&1 &
+#nohup python3 -m processing_pipeline.s3_tactic_extraction.runner_2 > data/processing_logs/s3_techlab.txt 2>&1 &
 
 
 
