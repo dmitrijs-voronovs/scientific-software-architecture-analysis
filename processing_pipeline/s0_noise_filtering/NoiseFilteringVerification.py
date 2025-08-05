2c15f99d64a637b90481b37f2db696262f13948c
dmitrijs-voronovs <vdmitryy@gmail.com>
2025-08-05 20:56:13 +0200

    Update verification generic prompt

--- processing_pipeline/s0_noise_filtering/NoiseFilteringVerification.py
+++ processing_pipeline/s0_noise_filtering/NoiseFilteringVerification.py
@@ -17,7 +17,7 @@ class NoiseFilteringStageVerification(IStageVerification):
 
 
 def main():
-    NoiseFilteringStageVerification(hostname=LLMHost.GREEN_LAB, batch_size_override=20).execute_verification()
+    NoiseFilteringStageVerification(hostname=LLMHost.TECH_LAB, batch_size_override=20).execute_verification()
 
 
 if __name__ == "__main__":
