2dc07ac01b9cab81e514f343c01dc975acaf4f06
dmitrijs-voronovs <vdmitryy@gmail.com>
2025-08-12 01:51:10 +0200

    Fix data optimizer, optimize data for stage 2 re-processing, update processing configs, launch processing

--- exec_stage.sh
+++ exec_stage.sh
@@ -1,14 +1,14 @@
 #! /bin/bash
 
 . .venv/bin/activate
-#nohup python3 -m processing_pipeline.s1_qa_relevance_check.QARelevanceCheck > data/processing_logs/s1_greenlab.txt 2>&1 &
-#nohup python3 -m processing_pipeline.s1_qa_relevance_check.runner_2 > data/processing_logs/s1_techlab.txt 2>&1 &
+nohup python3 -m processing_pipeline.s1_qa_relevance_check.QARelevanceCheck_v2 > data/processing_logs/v2_s1_greenlab.txt 2>&1 &
+nohup python3 -m processing_pipeline.s1_qa_relevance_check.runner_2 > data/processing_logs/v2_s1_techlab.txt 2>&1 &
 
-#nohup python3 -m processing_pipeline.s2_arch_relevance_check.ArchRelevanceCheck > data/processing_logs/s2_greenlab.txt 2>&1 &
-#nohup python3 -m processing_pipeline.s2_arch_relevance_check.runner_2 > data/processing_logs/s2_techlab.txt 2>&1 &
+nohup python3 -m processing_pipeline.s2_arch_relevance_check.ArchRelevanceCheck_v2 > data/processing_logs/v2_s2_greenlab.txt 2>&1 &
+nohup python3 -m processing_pipeline.s2_arch_relevance_check.runner_2 > data/processing_logs/v2_s2_techlab.txt 2>&1 &
 
-#nohup python3 -m processing_pipeline.s3_tactic_extraction.TacticExtraction > data/processing_logs/s3_greenlab.txt 2>&1 &
-#nohup python3 -m processing_pipeline.s3_tactic_extraction.runner_2 > data/processing_logs/s3_techlab.txt 2>&1 &
+nohup python3 -m processing_pipeline.s3_tactic_extraction.TacticExtraction_v2 > data/processing_logs/v2_s3_greenlab.txt 2>&1 &
+nohup python3 -m processing_pipeline.s3_tactic_extraction.runner_2 > data/processing_logs/v2_s3_techlab.txt 2>&1 &
 
 
 
