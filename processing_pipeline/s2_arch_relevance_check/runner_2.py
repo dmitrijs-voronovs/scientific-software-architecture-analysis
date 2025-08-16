7f8d8ea4088c80812b0936d4294deae3ef95bb14
dmitrijs-voronovs <vdmitryy@gmail.com>
2025-08-16 13:49:04 +0200

    Update runners for s2

--- processing_pipeline/s2_arch_relevance_check/runner_2.py
+++ processing_pipeline/s2_arch_relevance_check/runner_2.py
@@ -1,9 +1,11 @@
 from cfg.LLMHost import LLMHost
-from processing_pipeline.s2_arch_relevance_check.ArchRelevanceCheck import ArchitectureRelevanceCheckStage
+from processing_pipeline.s2_arch_relevance_check.ArchRelevanceCheck_v2 import ArchitectureRelevanceCheckStage_v2
 
 
 def main():
-    ArchitectureRelevanceCheckStage(hostname=LLMHost.SERVER).execute(["code_comment.", "issue."], reverse=True)
+    ArchitectureRelevanceCheckStage_v2(hostname=LLMHost.SERVER).execute(
+        ["root-project.root.v6-32-06.code_comment", "root-project.root.v6-32-06.docs",
+         "google.deepvariant.v1.6.1.issue_comment"], reverse=True)
 
 
 if __name__ == "__main__":
