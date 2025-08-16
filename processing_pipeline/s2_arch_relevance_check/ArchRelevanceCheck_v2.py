20ab4bb0216a80cb61a992cecc78128247cd010b
dmitrijs-voronovs <vdmitryy@gmail.com>
2025-08-16 17:14:20 +0200

    update batch size

--- processing_pipeline/s2_arch_relevance_check/ArchRelevanceCheck_v2.py
+++ processing_pipeline/s2_arch_relevance_check/ArchRelevanceCheck_v2.py
@@ -105,7 +105,7 @@ Now, apply the analysis steps defined in your system prompt to the data provided
 
 
 def main():
-    ArchitectureRelevanceCheckStage_v2(hostname=LLMHost.SERVER, n_threads_override=10, batch_size_override=10).execute(
+    ArchitectureRelevanceCheckStage_v2(hostname=LLMHost.SERVER, n_threads_override=10, batch_size_override=5).execute(
         ["root-project.root.v6-32-06.docs", "google.deepvariant.v1.6.1.issue_comment",
          "root-project.root.v6-32-06.code_comment.0.part_0.parquet",
          "root-project.root.v6-32-06.code_comment.0.part_1.parquet",
