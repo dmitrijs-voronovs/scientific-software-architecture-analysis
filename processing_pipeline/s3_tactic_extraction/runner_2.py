86638a7d28807f3ec54e70065dc778215422e7fd
dmitrijs-voronovs <vdmitryy@gmail.com>
2025-08-18 00:00:36 +0200

    Update s3 runners

--- processing_pipeline/s3_tactic_extraction/runner_2.py
+++ processing_pipeline/s3_tactic_extraction/runner_2.py
@@ -3,7 +3,10 @@ from processing_pipeline.s3_tactic_extraction.TacticExtraction_v2 import TacticE
 
 
 def main():
-    TacticExtractionStage_v2(hostname=LLMHost.SERVER).execute(["issue.", "docs"], reverse=True)
+    TacticExtractionStage_v2(hostname=LLMHost.SERVER, n_threads_override=6, batch_size_override=12).execute([
+        "allenai.scispacy.v0.5.5",
+        "OpenGene.fastp.v0.23.4"
+    ], reverse=False)
 
 
 if __name__ == "__main__":
