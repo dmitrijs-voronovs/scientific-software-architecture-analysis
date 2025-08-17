4cd011a359ca0cfe08a14ce97e80aa6e8c8914ac
dmitrijs-voronovs <vdmitryy@gmail.com>
2025-08-17 09:31:21 +0200

    Add functionality for sequentially splitting files, which help to parallelize  the processing of files that already in the middle of the pipeline

--- utilities/csv/split_file_into_n_parts.py
+++ utilities/csv/split_file_into_n_parts.py
@@ -10,20 +10,32 @@ def split_file(path: Path, n_parts: int, handler: IDfHandler):
     df = handler.read_df(path)
     for i in range(n_parts):
         handler.write_df(df.iloc[i::n_parts], path.with_stem(f"{path.stem}.part_{i:0{len(str(n_parts))}}"))
+    path.rename(path.with_stem(f"_{path.stem}"))
 
 def split_file_in_batches(path: Path, handler: IDfHandler, batch_size: int = 3000):
     df = handler.read_df(path)
     n_parts = math.ceil(df.shape[0] / batch_size)
     split_file(path, n_parts, handler)
 
+def split_file_in_sequential_batches(path: Path, handler: IDfHandler, batch_size: int = 3000):
+    df = handler.read_df(path)
+    path.rename(path.with_stem(f"_{path.stem}"))
+
+    n_parts = math.ceil(df.shape[0] / batch_size)
+    for i in range(n_parts):
+        start = i * batch_size
+        part_name = path if i == 0 else path.with_stem(f"{path.stem}.part_{i+1:0{len(str(n_parts))}}")
+        handler.write_df(df.iloc[start:start + batch_size], part_name)
+
 
 def main():
     # split_file(Path(r"C:\Users\Dmitrijs\Documents\myDocs\masters\courses\thesis\code\data\samples\verified\s0.csv"), 4, CSVDFHandler())
-    split_file_in_batches(Path(r"C:\Users\Dmitrijs\Documents\myDocs\masters\courses\thesis\code\data\keywords_2\s1_qa_relevance_check_o\root-project.root.v6-32-06.code_comment.0.parquet"), ParquetDFHandler())
-    split_file_in_batches(Path(r"C:\Users\Dmitrijs\Documents\myDocs\masters\courses\thesis\code\data\keywords_2\s1_qa_relevance_check_o\root-project.root.v6-32-06.code_comment.1.parquet"), ParquetDFHandler())
-    split_file_in_batches(Path(r"C:\Users\Dmitrijs\Documents\myDocs\masters\courses\thesis\code\data\keywords_2\s1_qa_relevance_check_o\root-project.root.v6-32-06.code_comment.2.parquet"), ParquetDFHandler())
-    split_file_in_batches(Path(r"C:\Users\Dmitrijs\Documents\myDocs\masters\courses\thesis\code\data\keywords_2\s1_qa_relevance_check_o\root-project.root.v6-32-06.code_comment.3.parquet"), ParquetDFHandler())
-    split_file_in_batches(Path(r"C:\Users\Dmitrijs\Documents\myDocs\masters\courses\thesis\code\data\keywords_2\s1_qa_relevance_check_o\root-project.root.v6-32-06.code_comment.4.parquet"), ParquetDFHandler())
+    # split_file_in_batches(Path(r"C:\Users\Dmitrijs\Documents\myDocs\masters\courses\thesis\code\data\keywords_2\s1_qa_relevance_check_o\root-project.root.v6-32-06.code_comment.0.parquet"), ParquetDFHandler())
+    # split_file_in_batches(Path(r"C:\Users\Dmitrijs\Documents\myDocs\masters\courses\thesis\code\data\keywords_2\s1_qa_relevance_check_o\root-project.root.v6-32-06.code_comment.1.parquet"), ParquetDFHandler())
+    # split_file_in_batches(Path(r"C:\Users\Dmitrijs\Documents\myDocs\masters\courses\thesis\code\data\keywords_2\s1_qa_relevance_check_o\root-project.root.v6-32-06.code_comment.2.parquet"), ParquetDFHandler())
+    # split_file_in_batches(Path(r"C:\Users\Dmitrijs\Documents\myDocs\masters\courses\thesis\code\data\keywords_2\s1_qa_relevance_check_o\root-project.root.v6-32-06.code_comment.3.parquet"), ParquetDFHandler())
+    # split_file_in_batches(Path(r"C:\Users\Dmitrijs\Documents\myDocs\masters\courses\thesis\code\data\keywords_2\s1_qa_relevance_check_o\root-project.root.v6-32-06.code_comment.4.parquet"), ParquetDFHandler())
+    split_file_in_sequential_batches(Path(r"C:\Users\Dmitrijs\Documents\myDocs\masters\courses\thesis\code\data\keywords_2\s2_arch_relevance_check_o\allenai.scispacy.v0.5.5.issue.parquet"), ParquetDFHandler(), 100)
 
 if __name__ == "__main__":
     main()
