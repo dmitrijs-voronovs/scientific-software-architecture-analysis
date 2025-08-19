651c1d063f3e402788844049c7daa892be80cc17
dmitrijs-voronovs <vdmitryy@gmail.com>
2025-08-19 02:16:12 +0200

    Optimize run configs

--- exec_stage.sh
+++ exec_stage.sh
@@ -5,9 +5,9 @@
 #nohup python3 -m processing_pipeline.s1_qa_relevance_check.runner_2 > data/processing_logs/v2_s1_techlab.1_5b.v4.txt 2>&1 &
 
 nohup python3 -m processing_pipeline.s2_arch_relevance_check.ArchRelevanceCheck_v2 > data/processing_logs/v2_s2_greenlab.v5.txt 2>&1 &
-nohup python3 -m processing_pipeline.s2_arch_relevance_check.runner_2 > data/processing_logs/v2_s2_techlab.v4.txt 2>&1 &
+nohup python3 -m processing_pipeline.s2_arch_relevance_check.runner_2 > data/processing_logs/v2_s2_techlab.v5.txt 2>&1 &
 
-nohup python3 -m processing_pipeline.s3_tactic_extraction.TacticExtraction_v2 > data/processing_logs/v2_s3_greenlab.txt 2>&1 &
+nohup python3 -m processing_pipeline.s3_tactic_extraction.TacticExtraction_v2 > data/processing_logs/v2_s3_greenlab.v3.txt 2>&1 &
 nohup python3 -m processing_pipeline.s3_tactic_extraction.runner_2 > data/processing_logs/v2_s3_techlab.v2.txt 2>&1 &
 
 
