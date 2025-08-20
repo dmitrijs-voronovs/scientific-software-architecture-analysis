1d04de29306a7b8a27199f176cc57aa8cdd35090
dmitrijs-voronovs <vdmitryy@gmail.com>
2025-08-20 15:07:31 +0200

    Flip runner config

--- exec_stage.sh
+++ exec_stage.sh
@@ -7,8 +7,8 @@
 nohup python3 -m processing_pipeline.s2_arch_relevance_check.ArchRelevanceCheck_v2 > data/processing_logs/v2_s2_greenlab.v5.txt 2>&1 &
 nohup python3 -m processing_pipeline.s2_arch_relevance_check.runner_2 > data/processing_logs/v2_s2_techlab.v5.txt 2>&1 &
 
-nohup python3 -m processing_pipeline.s3_tactic_extraction.TacticExtraction_v2 > data/processing_logs/v2_s3_greenlab.v6.txt 2>&1 &
-nohup python3 -m processing_pipeline.s3_tactic_extraction.runner_2 > data/processing_logs/v2_s3_techlab.v6.txt 2>&1 &
+nohup python3 -m processing_pipeline.s3_tactic_extraction.TacticExtraction_v2 > data/processing_logs/v2_s3_greenlab.v7.txt 2>&1 &
+nohup python3 -m processing_pipeline.s3_tactic_extraction.runner_2 > data/processing_logs/v2_s3_techlab.v7.txt 2>&1 &
 
 
 
